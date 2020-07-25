580c2baf4684705272336df6cd9707b2e55c5b37
Andrew Bereza <andrew@berezaagames.com>
2020-07-24 19:58:31 -0700

    initial commit

---
+++ src/ServerScriptService/contents/services/manager_crafting.lua
@@ -0,0 +1,31 @@
+local module = {}
+
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local modules = require(game.ReplicatedStorage:WaitForChild("modules"))
+local network = modules.load("network")
+
+local itemData = require(replicatedStorage:WaitForChild("itemLookup"))
+
+local function playerRequest_craftItem(player, itemId)
+	local item = itemData[itemId]
+	if item and item.recipe then
+		local success, reason = network:invoke(
+			"tradeItemsBetweenPlayerAndNPC",
+			player,
+			item.recipe,
+			0,
+			{id = item.id; stacks = 1},
+			0,
+			"etc:crafting"
+	)
+		return success, reason
+	end
+end
+
+local function main()
+	network:create("playerRequest_craftItem", "RemoteFunction", "OnServerInvoke", playerRequest_craftItem)
+end
+
+spawn(main)
+
+return module
