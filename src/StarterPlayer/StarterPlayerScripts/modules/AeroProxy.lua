5f21b667fbcb6ce9e17e5ec65ab5574b10e5854f
Andrew Bereza <andrew@berezaagames.com>
2020-07-25 20:51:47 -0700

    lower case N pt 2

--- src/StarterPlayer/StarterPlayerScripts/modules/AeroProxy.lua
+++ src/StarterPlayer/StarterPlayerScripts/modules/AeroProxy.lua
@@ -8,7 +8,7 @@
 	A proxy module to allow  main menu Aero code to interface with
 	the obfuscated network module. Only include functions that the
 	Aero-side will need.
-	
+
 	AeroProxy.getGameSaveData()
 	AeroProxy.renderCharacter(mask, appearanceData)
 
@@ -20,31 +20,31 @@ local AeroProxy = {}
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Modules = require((ReplicatedStorage.modules))
-local Network = Modules.load("network")
+local network = Modules.load("network")
 
 
 function AeroProxy.getGameSaveData(fileNum)
-	local globalDataSuccess, globalDataFromServer = Network:invokeServer("loadGame")
+	local globalDataSuccess, globalDataFromServer = network:invokeServer("loadGame")
 	return globalDataFromServer
 end
 
 
 function AeroProxy.renderCharacter(mask, appearanceData, player)
-	return Network:invoke("createRenderCharacterContainerFromCharacterAppearanceData", mask, appearanceData, player)
+	return network:invoke("createRenderCharacterContainerFromCharacterAppearanceData", mask, appearanceData, player)
 end
 
 
 function AeroProxy.getMovementAnimationForCharacter(character, animation)
 	local animationController 	= character.entity:WaitForChild("AnimationController")
-	local currentEquipment = Network:invoke("getCurrentlyEquippedForRenderCharacter", character.entity)
-	
+	local currentEquipment = network:invoke("getCurrentlyEquippedForRenderCharacter", character.entity)
+
 	local weaponType do
 		if currentEquipment[1] then
 			weaponType = currentEquipment[1].baseData.equipmentType
 		end
 	end
 
-	return Network:invoke("getMovementAnimationForCharacter", animationController, animation, weaponType, nil)
+	return network:invoke("getMovementAnimationForCharacter", animationController, animation, weaponType, nil)
 end
 
 return AeroProxy
